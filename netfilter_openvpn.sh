00626cde65e8f9a043d860109ef7a56f00409fd1
Guillaume Destuynder <kang@mozilla.com>
2014-05-13 17:41:43 -0700

    Fixes for environment variables

--- netfilter_openvpn.sh
+++ netfilter_openvpn.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 # This is also removed after everything has been successfull by the python script.
 # We remove on delete for safety in case the python script fails so that it doesn't forbid a potential future client.
+operation=$1
+address=$2
+# Use this if you need to use "username as common name". This has security implications:
+# username is arbitrary and sent by the user, common_name matches a signed certificate common bame and cannot be forged.
+#common_name=${username}
+
+SUDO=/usr/bin/sudo
+IPTABLES=/sbin/iptables
+
 if [[ ${operation} == "delete" ]]; then
-	iptables -D INPUT -s ${address} -j DROP
+	${SUDO} ${IPTABLES} -D INPUT -s ${address} -j DROP
 else
-	iptables -I INPUT -s ${address} -j DROP || {
+	${SUDO} ${IPTABLES} -I INPUT -s ${address} -j DROP || {
 		echo "Failed to run initial iptables command"
 		exit 127
 	}
 fi
 
-/usr/lib/openvpn/plugins/netfilter_openvpn.py &
+${SUDO} /usr/lib/openvpn/plugins/netfilter_openvpn.py ${operation} &
 disown
 exit 0
